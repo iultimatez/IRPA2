4d57fcd5ff6fb88b670f0dd088dd6e664ff424fd
iultimatez <appledynasty@gmail.com>
2014-10-27 01:12:22 +0800

    Finishing caculating tf-idf for each document

--- IRPA2.py
+++ IRPA2.py
@@ -1,10 +1,14 @@
 import re
 import porter_algorithm
+import math
 p = porter_algorithm.PorterStemmer()
 stop_words_file = open('stop_words.txt', 'r')
 stop_words = stop_words_file.read().split()
 fullList = []
-for x in range(1095):
+arrayForEachDocument = []
+arrayForEachDocumentWithCount = []
+numberOfDocs = 1095
+for x in range(numberOfDocs):
 	fileIndex = x + 1
 	file_name = "IRTM/" + str(fileIndex) + ".txt"
 	f = open (file_name, 'r')
@@ -13,8 +17,27 @@ for x in range(1095):
 	stemmed = map(lambda x: p.stem(x, 0, len(x)-1), stop)
 	addIndex = map(lambda x: {1: x, 2: fileIndex}, stemmed)
 	fullList += addIndex
-
+	addIndex.sort( key = lambda x : x[1])
+	sortedaddIndex = []
+	addIndexSortCount = -1
+	for z in range(len(addIndex)):
+		if  z == 0:
+			sortedaddIndex += [{1: addIndex[z][1], 2:1}]
+			addIndexSortCount += 1
+		else :
+			if addIndex[z][1] == addIndex[z-1][1]:
+				sortedaddIndex[addIndexSortCount][2] += 1
+			else :
+				sortedaddIndex += [{1: addIndex[z][1], 2:1}]
+				addIndexSortCount += 1
+	#print sortedaddIndex
+	arrayForEachDocumentWithCount.append(sortedaddIndex)
+	arrayForEachDocument.append(sorted(stemmed))
+	for q in sortedaddIndex:
+		print q[1] + '  ' + str(q[2])
 	f.close()
+print arrayForEachDocument
+print len(arrayForEachDocument)
 
 sortedList = sorted(fullList, key = lambda x : x[1])
 
@@ -23,7 +46,7 @@ dictCount = -1
 
 for x in range(len(sortedList)):
 	if  x == 0:
-		dictionary += [{1: sortedList[x][1], 2:1}]
+		dictionary += [{1: sortedList[x][1], 2:1, 3:dictCount+2}]
 		#print dictionary
 		dictCount += 1
 	else :
@@ -31,15 +54,31 @@ for x in range(len(sortedList)):
 			if sortedList[x][2] != sortedList[x-1][2]:
 				dictionary[dictCount][2] += 1
 		else :
-			dictionary += [{1: sortedList[x][1], 2:1}]
+			dictionary += [{1: sortedList[x][1], 2:1, 3:dictCount+2}]
 			dictCount += 1
+
 dictionaryTXT = open('dictionary.txt', 'w')
-print "{:<7} {:<20}{}".format("i_index", "term", "df")
+#print "{:<7} {:<20}{}".format("i_index", "term", "df")
 dictionaryTXT.write("{:<7} {:<20}{}\n".format("i_index", "term", "df")) 
 for x in range(len(dictionary)):
 	print "{:<5}   {:<20}{}".format(str(x+1), dictionary[x][1], str(dictionary[x][2])) 
-	dictionaryTXT.write("{:<5}   {:<20}{}\n".format(str(x+1), dictionary[x][1], str(dictionary[x][2])) )
+	dictionaryTXT.write("{:<5}   {:<20}{}\n".format(str(x+1), dictionary[x][1], str(dictionary[x][2])))
+
 #print len(dictionary)
+for y in range(len(arrayForEachDocument)):
+	getTermIndex = filter(lambda x: (x[1] in arrayForEachDocument[y]), dictionary)
+	fileIndex = y + 1
+	file_name = "results/" + str(fileIndex) + ".txt"
+	writeResult = open (file_name, 'w')
+	print file_name + ' ' + str(len(arrayForEachDocumentWithCount[y]))
+	writeResult.write(str(len(arrayForEachDocumentWithCount[y])) + '\n')
+	print "t_index tf_idf"
+	writeResult.write("t_index tf_idf\n")
+	for q in range(len(arrayForEachDocumentWithCount[y])):
+		print "{:<5}   {:.2f}".format(str(getTermIndex[q][3]), math.log(float(numberOfDocs) / float(getTermIndex[q][2]), 10) * arrayForEachDocumentWithCount[y][q][2])
+		writeResult.write("{:<5}   {:.2f}\n".format(str(getTermIndex[q][3]), math.log(float(numberOfDocs) / float(getTermIndex[q][2]), 10) * arrayForEachDocumentWithCount[y][q][2]))
+	writeResult.close()
+#str(getTermIndex[q][3]) + ' ' + arrayForEachDocumentWithCount[y][q][1] + "  " + str(math.log(float(numberOfDocs) / float(getTermIndex[q][2]), 10) * arrayForEachDocumentWithCount[y][q][2])
 dictionaryTXT.close()
 stop_words_file.close()
 
