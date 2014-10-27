60bc2bd7618a4a58f9c77e39655fb93d1e3f6fd5
iultimatez <appledynasty@gmail.com>
2014-10-27 22:55:15 +0800

    normalized tf-idf

--- IRPA2.py
+++ IRPA2.py
@@ -34,11 +34,11 @@ for x in range(numberOfDocs):
 	#print sortedaddIndex
 	arrayForEachDocumentWithCount.append(sortedaddIndex)
 	arrayForEachDocument.append(sorted(stemmed))
-	for q in sortedaddIndex:
-		print q[1] + '  ' + str(q[2])
+	#for q in sortedaddIndex:
+		#print q[1] + '  ' + str(q[2])
 	f.close()
-print arrayForEachDocument
-print len(arrayForEachDocument)
+#print arrayForEachDocument
+#print len(arrayForEachDocument)
 
 sortedList = sorted(fullList, key = lambda x : x[1])
 
@@ -62,27 +62,48 @@ dictionaryTXT = open('dictionary.txt', 'w')
 #print "{:<7} {:<20}{}".format("i_index", "term", "df")
 dictionaryTXT.write("{:<7} {:<20}{}\n".format("i_index", "term", "df")) 
 for x in range(len(dictionary)):
-	print "{:<5}   {:<20}{}".format(str(x+1), dictionary[x][1], str(dictionary[x][2])) 
+	#print "{:<5}   {:<20}{}".format(str(x+1), dictionary[x][1], str(dictionary[x][2])) 
 	dictionaryTXT.write("{:<5}   {:<20}{}\n".format(str(x+1), dictionary[x][1], str(dictionary[x][2])))
 
 allVectors = []
 #print len(dictionary)
 for y in range(len(arrayForEachDocument)):
 	getTermIndex = filter(lambda x: (x[1] in arrayForEachDocument[y]), dictionary)
-	fileIndex = y + 1
-	file_name = "results/" + str(fileIndex) + ".txt"
-	writeResult = open (file_name, 'w')
-	print file_name + ' ' + str(len(arrayForEachDocumentWithCount[y]))
-	writeResult.write(str(len(arrayForEachDocumentWithCount[y])) + '\n')
-	print "t_index tf_idf"
-	writeResult.write("t_index tf_idf\n")
+	# fileIndex = y + 1
+	# file_name = "results/" + str(fileIndex) + ".txt"
+	# writeResult = open (file_name, 'w')
+	# print file_name + ' ' + str(len(arrayForEachDocumentWithCount[y]))
+	# writeResult.write(str(len(arrayForEachDocumentWithCount[y])) + '\n')
+	# print "t_index tf_idf"
+	# writeResult.write("t_index tf_idf\n")
 	vector = []
 	for q in range(len(arrayForEachDocumentWithCount[y])):
-		print "{:<5}   {:.2f}".format(str(getTermIndex[q][3]), math.log(float(numberOfDocs) / float(getTermIndex[q][2]), 10) * arrayForEachDocumentWithCount[y][q][2])
-		writeResult.write("{:<5}   {:.2f}\n".format(str(getTermIndex[q][3]), math.log(float(numberOfDocs) / float(getTermIndex[q][2]), 10) * arrayForEachDocumentWithCount[y][q][2]))
+		#print "{:<5}   {:.2f}".format(str(getTermIndex[q][3]), math.log(float(numberOfDocs) / float(getTermIndex[q][2]), 10) * arrayForEachDocumentWithCount[y][q][2])
+		#writeResult.write("{:<5}   {:.2f}\n".format(str(getTermIndex[q][3]), math.log(float(numberOfDocs) / float(getTermIndex[q][2]), 10) * arrayForEachDocumentWithCount[y][q][2]))
 		vector.append({1: getTermIndex[q][3], 2: (math.log(float(numberOfDocs) / float(getTermIndex[q][2]), 10) * arrayForEachDocumentWithCount[y][q][2])})
-	writeResult.close()
+	#writeResult.close()
 	allVectors.append(vector)
+distanceForEveryDocument = []
+for x in range(len(allVectors)):
+	distance = 0.0
+	for y in range(len(allVectors[x])):
+		distance += allVectors[x][y][2]*allVectors[x][y][2]
+	distance = math.sqrt(distance)
+	distanceForEveryDocument.append(distance)
+#print distanceForEveryDocument
+for x in range(len(allVectors)):
+	fileIndex = x + 1
+	file_name = "results/" + str(fileIndex) + ".txt"
+	writeResult = open (file_name, 'w')
+	#print file_name + ' ' + str(len(allVectors[x]))
+	writeResult.write(str(len(allVectors[x])) + '\n')
+	#print "t_index tf_idf"
+	writeResult.write("t_index tf_idf\n")
+	for y in range(len(allVectors[x])):
+		allVectors[x][y][2] = allVectors[x][y][2]/distanceForEveryDocument[x]
+		#print "{:<5}   {:.2f}".format(str(allVectors[x][y][1]), allVectors[x][y][2])
+		writeResult.write("{:<5}   {:.2f}\n".format(str(allVectors[x][y][1]), allVectors[x][y][2]))
+	writeResult.close()
 def cos_similarity(x, y):
 	common = []
 	pointerX = 0
@@ -96,19 +117,20 @@ def cos_similarity(x, y):
 			pointerX += 1
 		elif allVectors[x][pointerX][1] > allVectors[y][pointerY][1]:
 			pointerY += 1
-	print common
-	distanceX = 0.0
-	distanceY = 0.0
-	for a in allVectors[x]:
-		distanceX += a[2]*a[2]
-	distanceX = math.sqrt(distanceX)
-	for b in allVectors[y]:
-		distanceY += b[2]*b[2]
-	distanceY = math.sqrt(distanceY)
+	#print common
+	# distanceX = 0.0
+	# distanceY = 0.0
+	# for a in allVectors[x]:
+	# 	distanceX += a[2]*a[2]
+	# distanceX = math.sqrt(distanceX)
+	# for b in allVectors[y]:
+	# 	distanceY += b[2]*b[2]
+	# distanceY = math.sqrt(distanceY)
 	inner = 0.0
 	for c in common:
 		inner += c[5]
-	return inner/(distanceY*distanceX)
+	#return inner/(distanceY*distanceX)
+	return inner
 #str(getTermIndex[q][3]) + ' ' + arrayForEachDocumentWithCount[y][q][1] + "  " + str(math.log(float(numberOfDocs) / float(getTermIndex[q][2]), 10) * arrayForEachDocumentWithCount[y][q][2])
 dictionaryTXT.close()
 stop_words_file.close()
